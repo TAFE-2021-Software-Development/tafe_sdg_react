dc1edcfabdbba135e391f7e4bdf19f30a9ad7a09
thepirateboy <rafee.sama@gmail.com>
2021-11-28 17:09:37 +1000

    Added axios for http request, move tips as alert at homescreen.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,144 +1,148 @@
-import React from "react";
-import {TouchableOpacity,Text,View,Image,TextInput}from 'react-native'
+import React from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TextInput,
+  SafeAreaView,
+} from 'react-native';
 import Checkbox from 'expo-checkbox';
 
-
 const Login = ({navigation}) => {
-    const [checked, setChecked] = React.useState(false);
-    return(
-        <View>
-            <Image source={require('../img/background2.png')}
-                style={{width:"100%",height:"100%"}}
-            />
-            <Text
-                style={{
-                    fontSize:60,
-                    fontWeight:"900",
-                    color:'white',
-                    alignSelf:'center',
-                    position:'absolute',
-                    top: 200,
-                }}
-            >
-                Welcome</Text>
-            <Text
-                style={{
-                fontSize:20,
-                fontWeight:"600",
-                color:'white',
-                alignSelf:'center',
-                position:'absolute',
-                top: 270,
-                }}
-            >
-             Lets sign you in</Text>
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <View>
+      <Image
+        source={require('../img/background2.png')}
+        style={{width: '100%', height: '100%'}}
+      />
+      <Text
+        style={{
+          fontSize: 60,
+          fontWeight: '900',
+          color: 'white',
+          alignSelf: 'center',
+          position: 'absolute',
+          top: 200,
+        }}>
+        Welcome
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: '600',
+          color: 'white',
+          alignSelf: 'center',
+          position: 'absolute',
+          top: 270,
+        }}>
+        Lets sign you in
+      </Text>
 
-             <View style={{
-                backgroundColor:"white",
-                alignSelf:'center',
-                borderRadius:15,
-                position:'absolute',
-                top: 400,
-             }}>
-                 <TextInput
-                    style={{
-                        height: 40,
-                        width:300,
-                        padding:10,
-                        color:'#00716F',
-                    }}
-                    placeholder="email/username"
-                />
-             </View>
-            
-             <View style={{
-                backgroundColor:"white",
-                alignSelf:'center',
-                borderRadius:15,
-                position:'absolute',
-                top: 460,
-             }}>
-                 <TextInput
-                    style={{
-                        height: 40,
-                        width:300,
-                        padding:10,
-                        color:'#00716F',
-                    }}
-                    placeholder="Password"
-                />
-             </View>
-            <View 
-                style={{
-                    top:510,
-                    left:40,
-                    position:'absolute',
-                    flexDirection:'row'
-                }}>
-                <Checkbox
-                    value={checked}
-                    onValueChange={setChecked}
-                    style={{
-                        width:13,
-                        height:13,
-                        margin:3
-                        
-                    }}
-                />
-                <Text style={{color:'white',fontSize:13}}>Remember Me</Text>
-                
-            </View>
-            <View style={{
-                    top: 510,
-                    right:40,
-                    color:'white',
-                    position:'absolute'
-                }}>
-                <Text 
-                style={{color:'white',fontSize:13}}
-                onPress={ () => navigation.navigate('Main') }
-                >Forgot password?</Text>
-            </View>
-           
-            
- 
+      <View
+        style={{
+          backgroundColor: 'white',
+          alignSelf: 'center',
+          borderRadius: 15,
+          position: 'absolute',
+          top: 400,
+        }}>
+        <TextInput
+          style={{
+            height: 40,
+            width: 300,
+            padding: 10,
+            color: '#00716F',
+          }}
+          placeholder="email/username"
+        />
+      </View>
 
-            <TouchableOpacity 
-            onPress={ () => navigation.navigate('Main') }
-            style={{
-                paddingHorizontal:120,
-                paddingVertical:10,
-                alignSelf:'center',
-                backgroundColor:"#00716F",
-                borderRadius:15,
-                position:'absolute',
-                bottom: 150,
-            }}>
-                <Text 
-                onPress={ () => navigation.navigate('Main') }
-                style={{color:"white",fontSize:20}}>
-                    Login
-                </Text>
-            </TouchableOpacity>
+      <View
+        style={{
+          backgroundColor: 'white',
+          alignSelf: 'center',
+          borderRadius: 15,
+          position: 'absolute',
+          top: 460,
+        }}>
+        <TextInput
+          style={{
+            height: 40,
+            width: 300,
+            padding: 10,
+            color: '#00716F',
+          }}
+          placeholder="Password"
+        />
+      </View>
+      <View
+        style={{
+          top: 510,
+          left: 40,
+          position: 'absolute',
+          flexDirection: 'row',
+        }}>
+        <Checkbox
+          value={checked}
+          onValueChange={setChecked}
+          style={{
+            width: 13,
+            height: 13,
+            margin: 3,
+          }}
+        />
+        <Text style={{color: 'white', fontSize: 13}}>Remember Me</Text>
+      </View>
+      <View
+        style={{
+          top: 510,
+          right: 40,
+          color: 'white',
+          position: 'absolute',
+        }}>
+        <Text
+          style={{color: 'white', fontSize: 13}}
+          onPress={() => navigation.navigate('Main')}>
+          Forgot password?
+        </Text>
+      </View>
 
-            <Text
-                onPress={ () => navigation.navigate('Register') }
-                style={{
-                    fontSize:20,
-                    fontWeight:"300",
-                    color:'white',
-                    alignSelf:'center',
-                    position:'absolute',
-                    bottom: 100,
-                }}
-                onPress={ () => navigation.navigate('Register') }
-            >
-                Don't have an account? Sign up</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Main')}
+        style={{
+          paddingHorizontal: 120,
+          paddingVertical: 10,
+          alignSelf: 'center',
+          backgroundColor: '#00716F',
+          borderRadius: 15,
+          position: 'absolute',
+          bottom: 150,
+        }}>
+        <Text
+          onPress={() => navigation.navigate('Main')}
+          style={{color: 'white', fontSize: 20}}>
+          Login
+        </Text>
+      </TouchableOpacity>
 
-        </View>
-    )
-}
+      <Text
+        onPress={() => navigation.navigate('Register')}
+        style={{
+          fontSize: 20,
+          fontWeight: '300',
+          color: 'white',
+          alignSelf: 'center',
+          position: 'absolute',
+          bottom: 100,
+        }}
+        onPress={() => navigation.navigate('Register')}>
+        Don't have an account? Sign up
+      </Text>
+    </View>
+  );
+};
 
-
-
-export default Login
+export default Login;
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,275 +1,303 @@
-import React,{useCallback} from "react";
-import {Button,Linking,Text,View,Image,TouchableOpacity} from 'react-native'
+import React, {useCallback} from 'react';
+import {
+  Button,
+  Linking,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import axios from 'axios';
 
-
-
-const Main = ({navigation}) => {
-    return(
-        <View>
-            <Image source={require('../img/background2.png')}
-                style={{width:"100%",height:"100%"}}
-            />
-             
-            <Text
-                style={{
-                    fontSize:40,
-                    fontWeight:"900",
-                    color:'white',
-                    position:'absolute',
-                    top: 80,
-                    left: 10,
-                }}
-            >
-                Explore
-            </Text>
-          
-         
-            <TouchableOpacity 
-            onPress={ () => navigation.navigate('Cafe') }
-            style={{
-                height:120,
-                width:100,
-                borderRadius:120/2,
-                position:'absolute',
-                top: 150,
-                left:10,
-            }}
-            >
-            <Image source={require('../img/Cafe.png')}
-                    style={{width:"100%",height:"100%"}}
-                />
-    
-            </TouchableOpacity>
-
-            <TouchableOpacity 
-            onPress={ () => navigation.navigate('Events') }
-            style={{
-                height:120,
-                width:100,
-                borderRadius:120/2,
-                position:'absolute',
-                top: 150,
-                left:140,
-            }}
-            >
-            <Image source={require('../img/Events.png')}
-                    style={{width:"100%",height:"100%"}}
-                />
-    
-            </TouchableOpacity>
-            
-            <TouchableOpacity style={{
-                
-                position:'absolute',
-                top: 150,
-                left:270,
-                alignItems:'center',
-                justifyContent:'center'
-            }}
-            onPress={ () => navigation.navigate('Loyalty') }
-            >
-                 <Image source={require('../img/loyalty.jpg')}
-                    style={{height:105,
-                        width:105,
-                        borderRadius:105/2,}}
-                />
-                <Text 
-
-                
-                style={{color:"white",fontSize:14,marginTop:1}}>
-                    Loyalty
-                </Text>
-            </TouchableOpacity>
-            <Text
-            
-                style={{
-                    fontSize:30,
-                    fontWeight:"900",
-                    color:'white',
-                    position:'absolute',
-                    top: 300,
-                    left: 10,
-                }}
-            >
-                Sustainability Goals
-            </Text>
-            <TouchableOpacity 
-            onPress={ () => navigation.navigate('Target') }
-            style={{
-                height:100,
-                width:90,
-                borderRadius:100/2,
-                backgroundColor:"#00716F",
-                position:'absolute',
-                top: 350,
-                left:50,
-                alignItems:'center', 
-                justifyContent:'center',
-                flex:1
-            }}
-            >
-            <Image source={require('../img/target.png')}
-                    style={{width:"60%",height:"60%"}}
-                />
-            <Text                
-            style={{
-                    fontSize:14,
-                    fontWeight:'600',
-                    color:'white',
-                    position:'absolute',
-                    top: 100,
-                    left: 10,
-                }}>
-                Tafe QLD Targets
-            </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity 
-            onPress={ () => navigation.navigate('SDG') }
-            style={{
-                height:100,
-                width:90,
-                borderRadius:100/2,
-                backgroundColor:"#00716F",
-                position:'absolute',
-                top: 350,
-                left:220,
-                alignItems:'center', 
-                justifyContent:'center',
-                flex:1
-            }}
-            >
-            <Image source={require('../img/Goals.png')}
-                    style={{width:"100%",height:"100%"}}
-                />
-            <Text                
-            style={{
-                    fontSize:14,
-                    fontWeight:'600',
-                    color:'white',
-                    position:'absolute',
-                    top: 100,
-                    left: 10,
-                }}>
-                SDG Goals
-            </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity 
-            onPress={ () => navigation.navigate('Challenge') }
-            style={{
-                height:100,
-                width:90,
-                borderRadius:100/2,
-                backgroundColor:"#00716F",
-                position:'absolute',
-                top: 500,
-                left:50,
-                alignItems:'center', 
-                justifyContent:'center',
-                flex:1
-            }}
-            >
-            <Image source={require('../img/Challenge.png')}
-                    style={{width:"80%",height:"80%"}}
-                />
-            <Text                
-            style={{
-                    fontSize:14,
-                    fontWeight:'600',
-                    color:'white',
-                    position:'absolute',
-                    top: 100,
-                    left: 10,
-                }}>
-                Challenge
-            </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity 
-            onPress={ () => navigation.navigate('Tips') }
-            style={{
-                height:100,
-                width:90,
-                borderRadius:100/2,
-                backgroundColor:"#00716F",
-                position:'absolute',
-                top: 500,
-                left:220,
-                alignItems:'center', 
-                justifyContent:'center',
-                flex:1
-            }}
-            >
-            <Image source={require('../img/Tips.png')}
-                    style={{width:"70%",height:"70%",position:'absolute',  left:5,}}
-                />
-            <Text                
-            style={{
-                    fontSize:14,
-                    fontWeight:'600',
-                    color:'white',
-                    position:'absolute',
-                    top: 100,
-                    left: 25,
-                }}>
-                Tips
-            </Text>
-            </TouchableOpacity>
-
-            <View style={{
-            backgroundColor:'black',
-            position:'absolute',
-            bottom:0,
-            height:50,
-            width:'100%'
-        }}>
-            <TouchableOpacity style={{
-                position:'absolute',
-                top:5,
-                left:50,
-                height:30,
-                width:30,
-            }}
-            onPress={ () => navigation.navigate('User') }
-            >
-            <Image source={require('../img/userlogo.png')}
-                style={{width:"100%",height:"100%"}}
-            />
-            </TouchableOpacity>
-
-            <TouchableOpacity  style={{
-                position:'absolute',
-                top:5,
-                left:180,
-                height:30,
-                width:30,
-            }}
-            onPress={ () => navigation.navigate('Main') }>
-            <Image source={require('../img/homelogo.png')}
-                style={{width:"100%",height:"100%"}}
-            />
-                
-            </TouchableOpacity>
-
-            <TouchableOpacity style={{
-                position:'absolute',
-                top:5,
-                left:300,
-                height:30,
-                width:30,
-            }}
-            onPress={ () => Linking.openURL("https://tafeqld.edu.au/") }>
-            <Image source={require('../img/tafe-logo.png')}
-                style={{width:"100%",height:"100%"}}
-            />
-            
-            </TouchableOpacity>
-
-        </View>
-        </View>
-    )
+async function getTips() {
+  try {
+    const response = await axios.get('http://3.16.123.62/api/tip/get/');
+    const finalResponse = response.data.Tip.tip;
+    console.log(finalResponse);
+    return await finalResponse;
+  } catch (error) {
+    console.error(error);
+    return await error;
+  }
 }
 
+async function alertHandler(navigation) {
+  const response = await getTips();
+  //function to make three option alert
+  Alert.alert(
+    //title
+    `Today's Tips`,
+    //body
+    `${response}`,
+    [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+    {cancelable: true},
+  );
+}
 
-export default Main
+const Main = ({navigation}) => {
+  return (
+    <View>
+      <Image
+        source={require('../img/background2.png')}
+        style={{width: '100%', height: '100%'}}
+      />
+
+      <Text
+        style={{
+          fontSize: 40,
+          fontWeight: '900',
+          color: 'white',
+          position: 'absolute',
+          top: 80,
+          left: 10,
+        }}>
+        Explore
+      </Text>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Cafe')}
+        style={{
+          height: 120,
+          width: 100,
+          borderRadius: 120 / 2,
+          position: 'absolute',
+          top: 150,
+          left: 10,
+        }}>
+        <Image
+          source={require('../img/Cafe.png')}
+          style={{width: '100%', height: '100%'}}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Events')}
+        style={{
+          height: 120,
+          width: 100,
+          borderRadius: 120 / 2,
+          position: 'absolute',
+          top: 150,
+          left: 140,
+        }}>
+        <Image
+          source={require('../img/Events.png')}
+          style={{width: '100%', height: '100%'}}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          top: 150,
+          left: 270,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={() => navigation.navigate('Loyalty')}>
+        <Image
+          source={require('../img/loyalty.jpg')}
+          style={{height: 105, width: 105, borderRadius: 105 / 2}}
+        />
+        <Text style={{color: 'white', fontSize: 14, marginTop: 1}}>
+          Loyalty
+        </Text>
+      </TouchableOpacity>
+
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: '900',
+          color: 'white',
+          position: 'absolute',
+          top: 300,
+          left: 10,
+        }}>
+        Sustainability Goals
+      </Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Target')}
+        style={{
+          height: 100,
+          width: 90,
+          borderRadius: 100 / 2,
+          backgroundColor: '#00716F',
+          position: 'absolute',
+          top: 350,
+          left: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+        <Image
+          source={require('../img/target.png')}
+          style={{width: '60%', height: '60%'}}
+        />
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: '600',
+            color: 'white',
+            position: 'absolute',
+            top: 100,
+            left: 10,
+          }}>
+          Tafe QLD Targets
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('SDG')}
+        style={{
+          height: 100,
+          width: 90,
+          borderRadius: 100 / 2,
+          backgroundColor: '#00716F',
+          position: 'absolute',
+          top: 350,
+          left: 220,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+        <Image
+          source={require('../img/Goals.png')}
+          style={{width: '100%', height: '100%'}}
+        />
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: '600',
+            color: 'white',
+            position: 'absolute',
+            top: 100,
+            left: 10,
+          }}>
+          SDG Goals
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Challenge')}
+        style={{
+          height: 100,
+          width: 90,
+          borderRadius: 100 / 2,
+          backgroundColor: '#00716F',
+          position: 'absolute',
+          top: 500,
+          left: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+        <Image
+          source={require('../img/Challenge.png')}
+          style={{width: '80%', height: '80%'}}
+        />
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: '600',
+            color: 'white',
+            position: 'absolute',
+            top: 100,
+            left: 10,
+          }}>
+          Challenge
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          alertHandler();
+        }}
+        style={{
+          height: 100,
+          width: 90,
+          borderRadius: 100 / 2,
+          backgroundColor: '#00716F',
+          position: 'absolute',
+          top: 500,
+          left: 220,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+        <Image
+          source={require('../img/Tips.png')}
+          style={{width: '70%', height: '70%', position: 'absolute', left: 5}}
+        />
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: '600',
+            color: 'white',
+            position: 'absolute',
+            top: 100,
+            left: 25,
+          }}>
+          Tips
+        </Text>
+      </TouchableOpacity>
+
+      <View
+        style={{
+          backgroundColor: 'black',
+          position: 'absolute',
+          bottom: 0,
+          height: 50,
+          width: '100%',
+        }}>
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            top: 5,
+            left: 50,
+            height: 30,
+            width: 30,
+          }}
+          onPress={() => navigation.navigate('User')}>
+          <Image
+            source={require('../img/userlogo.png')}
+            style={{width: '100%', height: '100%'}}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            top: 5,
+            left: 180,
+            height: 30,
+            width: 30,
+          }}
+          onPress={() => navigation.navigate('Main')}>
+          <Image
+            source={require('../img/homelogo.png')}
+            style={{width: '100%', height: '100%'}}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            top: 5,
+            left: 300,
+            height: 30,
+            width: 30,
+          }}
+          onPress={() => Linking.openURL('https://tafeqld.edu.au/')}>
+          <Image
+            source={require('../img/tafe-logo.png')}
+            style={{width: '100%', height: '100%'}}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Main;
